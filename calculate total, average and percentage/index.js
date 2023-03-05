7ac8911a1fae37d57486b14eb2280cee3beb9ec5
abdelrouf-ali <84926894+abdelrouf-ali@users.noreply.github.com>
2023-03-05 14:10:04 +0200

    Add files via upload

---
+++ calculate total, average and percentage/index.js
@@ -0,0 +1,12 @@
+
+var mark1 = prompt("Enter the subject1 mark");
+var mark2 = prompt("Enter the subject2 mark");
+var mark3 = prompt("Enter the subject3 mark");
+var mark4 = prompt("Enter the subject4 mark");
+var mark5 = prompt("Enter the subject5 mark");
+
+
+Total =  Number(mark1) +  Number(mark2) +  Number(mark3) +  Number(mark4) +  Number(mark5);
+avg = Total/5;
+
+alert( "Total: " + Total + "     avg:  " + avg + "     Percentage:  " + avg + "%" );
